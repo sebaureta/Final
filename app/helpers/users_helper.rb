3edd07fef7c215d8bcf71e53354a8f4f6be31639
sebaureta <sureta@outlook.es>
2019-03-26 16:17:17 -0300

    added navbar and user settings, permited params at app controller

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,10 @@
 module UsersHelper
-  def avatar_for(user)
+  def avatar_for(user, options = { size: 80 })
+    size = options[:size]
     if user.avatar?
-      image_tag user.avatar.url(:thumb)
+      image_tag user.avatar.url(:thumb), width: size
     else
-      image_tag "some images"
+      image_tag 'default-avatar.png', width: size
     end
   end
 end
