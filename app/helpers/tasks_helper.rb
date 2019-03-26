ca29261a07a85d67942d0a2776038530863b05ab
sebaureta <sureta@outlook.es>
2019-03-26 16:46:37 -0300

    added helper for task + uploading images ans showing it at index in a thumbnail

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,10 @@
 module TasksHelper
+  def image_for(task, options = { size: 300 })
+  size = options[:size]
+    if task.image?
+      image_tag task.image.url(:thumb), width: size
+    else
+      image_tag 'default-avatar.png', width: size
+    end
+end
 end
